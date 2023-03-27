9a31babd27532ed8cf1e8a74fcec3058516111a3
nicholas365 <robern3@mcmaster.ca>
2023-03-27 19:30:57 -0400

    Reset test file

--- src/mergeConflictTest.py
+++ src/mergeConflictTest.py
@@ -2,15 +2,12 @@ import abc
 import babel
 import cachetools
 import dataclasses
-import filecmp
 import email
-import zipapp
 
 list = []
 def helloWorld():
     main = 5 + 6
     print("Hello World!")
     main += 1
-    list.append(main)
 
 helloWorld()
