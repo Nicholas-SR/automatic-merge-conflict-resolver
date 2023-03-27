6c204247afeefeb56cafc1fbc25157bdb01c78ec
nicholas365 <robern3@mcmaster.ca>
2023-03-27 18:56:48 -0400

    test

--- src/mergeConflictTest.py
+++ src/mergeConflictTest.py
@@ -2,12 +2,15 @@ import abc
 import babel
 import cachetools
 import dataclasses
+import filecmp
 import email
+import zipapp
 
 list = []
 def helloWorld():
     main = 5 + 6
     print("Hello World!")
     main += 1
+    list.append(main)
 
 helloWorld()
