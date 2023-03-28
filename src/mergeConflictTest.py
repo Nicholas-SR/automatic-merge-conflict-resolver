30a21ce9881a9e7a4dab4136643dde3b964852a4
nicholas365 <robern3@mcmaster.ca>
2023-03-27 20:24:59 -0400

    test

--- src/mergeConflictTest.py
+++ src/mergeConflictTest.py
@@ -14,11 +14,13 @@ def helloWorld():
     print("Hello World!")
     main += 1
     list.append(1)
+    list.append(2)
 
 helloWorld()
 
 list2 = []
 def helloWorld2():
+    # a
     main = 5 + 6
     print("Hello World!")
     main += 1
